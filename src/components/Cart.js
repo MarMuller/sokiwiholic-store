7cfc84bc9b98bfd2fed238cbca0eef6053aa2b5d
Marina A. Muller <muller.marina@hotmail.com>
2021-11-08 05:05:33 -0300

    Item loop

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,21 @@ import Item from './Cart_Items';
 import '../styles/main-styles.css';
 import '../styles/cart-style.css';
 
+let items=[
+{key:"1", image:"", name:"Zapatos rojos", description:"Lorem ipsum dolor", stock:"1", initial:"1", price:"1000"},
+{key:"2", image:"", name:"Vestido azul", description:"Lorem ipsum dolor", stock:"2", initial:"2", price:"1500"},
+{key:"3", image:"", name:"Anteojos de sol", description:"Lorem ipsum dolor", stock:"1", initial:"3", price:"300"},
+{key:"4", image:"", name:"Stickers", description:"Lorem ipsum dolor", stock:"20", initial:"4", price:"25"}
+];
+
+let itemList=[];
+
+for (var item of items) {
+  itemList.push(
+    <Item key={item.key} image={item.image} name={item.name} description={item.description} stock={item.stock} initial={item.initial} price={item.price}/>
+  )
+}
+
 
 class Cart extends React.Component {
   render() {
@@ -10,10 +25,7 @@ class Cart extends React.Component {
       <>
 
         <div id="componente__carrito">
-          <Item image="" name="Zapatos rojos" description="Lorem ipsum dolor" stock="1" initial="1" price="1000"/>
-          <Item image="" name="Vestido azul" description="Lorem ipsum dolor" stock="2" initial="2" price="1500"/>
-          <Item image="" name="Anteojos de sol" description="Lorem ipsum dolor" stock="1" initial="3" price="300"/>
-          <Item image="" name="Stickers" description="Lorem ipsum dolor" stock="20" initial="4" price="25"/>
+          {itemList}
         </div>
 
       </>
