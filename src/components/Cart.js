bd026e8862ac0e62cc580379d2749b92a92b5fb0
Marina A. Muller <muller.marina@hotmail.com>
2021-11-15 01:07:28 -0300

    item key map function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,19 +4,25 @@ import '../styles/main-styles.css';
 import '../styles/cart-style.css';
 
 let items=[
-{key:"1", image:"", name:"Zapatos rojos", description:"Lorem ipsum dolor", stock:"2", initial:"1", price:"1000"},
-{key:"2", image:"", name:"Vestido azul", description:"Lorem ipsum dolor", stock:"5", initial:"1", price:"1500"},
-{key:"3", image:"", name:"Anteojos de sol", description:"Lorem ipsum dolor", stock:"1", initial:"1", price:"300"},
-{key:"4", image:"", name:"Stickers", description:"Lorem ipsum dolor", stock:"20", initial:"1", price:"25"}
+{image:"", name:"Zapatos rojos", description:"Lorem ipsum dolor", stock:"2", initial:"1", price:"1000"},
+{image:"", name:"Vestido azul", description:"Lorem ipsum dolor", stock:"5", initial:"1", price:"1500"},
+{image:"", name:"Anteojos de sol", description:"Lorem ipsum dolor", stock:"1", initial:"1", price:"300"},
+{image:"", name:"Stickers", description:"Lorem ipsum dolor", stock:"20", initial:"1", price:"25"}
 ];
 
-let itemList=[];
-
-for (var item of items) {
-  itemList.push(
-    <Item key={item.key} image={item.image} name={item.name} description={item.description} stock={item.stock} initial={item.initial} price={item.price}/>
+var itemList = items.map(function(item, i) {
+  return (
+    <Item key={i} image={item.image} name={item.name} description={item.description} stock={item.stock} initial={item.initial}  price={item.price}/>
   )
-}
+});
+
+// let itemList=[];
+//
+// for (var item of items) {
+//   itemList.push(
+//     <Item image={item.image} name={item.name} description={item.description} stock={item.stock} initial={item.initial} price={item.price}/>
+//   )
+// }
 
 
 class Cart extends React.Component {
